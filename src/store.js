27896b61a9c53cda34c5d6c147f39383fc6833ec
Yahya Rouissi <yahyarouissi@gmail.com>
2024-08-21 12:53:57 +0300

    päivitetty 'store' tilalla 'configureStore'.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux'
-import rootReducer from './reducers/combinedReducer'
+import { configureStore } from '@reduxjs/toolkit'
+import anecdoteReducer from './features/anecdote/anecdoteSlice'
+import filterReducer from './features/filter/filterSlice'
 
-const store = createStore(rootReducer)
+const store = configureStore({
+    reducer: {
+        anecdote: anecdoteReducer,
+        filter: filterReducer,
+    }
+})
 
 export default store
